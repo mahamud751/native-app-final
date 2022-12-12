2e49b64363d6e4e40315d400203e15c5c5d76702
mahamud751 <mahamudpino24678@gmail.com>
2022-12-12 12:23:03 +0600

    added new server

--- src/components/FlashSale.jsx
+++ src/components/FlashSale.jsx
@@ -1,87 +1,99 @@
-import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { add_item } from '../../redux/actions/cartAction'
-import { useNavigation } from '@react-navigation/native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { add_item } from "../../redux/actions/cartAction";
+import { useNavigation } from "@react-navigation/native";
 
 export default function FlashSale({ navigation }) {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        fetch('https://guarded-garden-69209.herokuapp.com/flashSales')
-            .then(res => res.json())
-            .then(data => {
-                setProducts(data)
-            })
-    }, [products])
-    const getState = useSelector((state) => state.cartReducer.cart)
-    const dispatch = useDispatch()
-    const handleAddItem = (e) => {
-        dispatch(add_item(e))
-    }
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://native-admin.onrender.com/flashSales")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [products]);
+  const getState = useSelector((state) => state.cartReducer.cart);
+  const dispatch = useDispatch();
+  const handleAddItem = (e) => {
+    dispatch(add_item(e));
+  };
 
-
-    const navigator = useNavigation()
-    return (
-
+  const navigator = useNavigation();
+  return (
+    <View>
+      <View>
         <View>
-            <View>
-                <View>
-                    <Text style={styles.titleSection}>Flash Sale</Text>
-                </View>
-
-
-                <FlatList
-                    data={products}
-                    horizontal
-
-
-                    keyExtractor={(key) => key._id}
-                    renderItem={({ item }) => {
-                        return (
-                            <View style={{ marginRight: 20, marginVertical: 10 }}>
-                                <View>
-                                    <Image source={{ uri: item.img }} style={styles.image} />
-                                </View>
-                                <View >
-                                    <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{item.name}</Text>
-                                    <Text style={{ fontSize: 14, fontWeight: '500', marginVertical: 6 }}>৳{item.price}</Text>
-                                    <TouchableOpacity
-                                        onPress={() => navigator.navigate('Details', { item }, handleAddItem(item))}
-                                        style={{ padding: 10, backgroundColor: 'red', borderRadius: 10, }}
-                                    >
-                                        <Text style={{ textAlign: 'center', color: 'white' }} >Buy Now</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        )
-                    }}
-
-                />
-
-
-
-
-
-
-
-
-
-            </View>
+          <Text style={styles.titleSection}>Flash Sale</Text>
         </View>
-    )
+
+        <FlatList
+          data={products}
+          horizontal
+          keyExtractor={(key) => key._id}
+          renderItem={({ item }) => {
+            return (
+              <View style={{ marginRight: 20, marginVertical: 10 }}>
+                <View>
+                  <Image source={{ uri: item.img }} style={styles.image} />
+                </View>
+                <View>
+                  <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+                    {item.name}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "500",
+                      marginVertical: 6,
+                    }}
+                  >
+                    ৳{item.price}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() =>
+                      navigator.navigate(
+                        "Details",
+                        { item },
+                        handleAddItem(item)
+                      )
+                    }
+                    style={{
+                      padding: 10,
+                      backgroundColor: "red",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Text style={{ textAlign: "center", color: "white" }}>
+                      Buy Now
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            );
+          }}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    titleSection: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#096266'
-    },
-    image: {
-        width: 90,
-        height: 90,
-        borderRadius: 10,
-    },
-
-
-})
+  titleSection: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#096266",
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 10,
+  },
+});
--- src/screens/Orders.jsx
+++ src/screens/Orders.jsx
@@ -1,97 +1,88 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { DataTable } from 'react-native-paper';
-import { getAuth } from 'firebase/auth';
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { DataTable } from "react-native-paper";
+import { getAuth } from "firebase/auth";
 const optionsPerPage = [2, 3, 4];
 export default function Orders() {
-    const auth = getAuth();
-    const user = auth.currentUser
+  const auth = getAuth();
+  const user = auth.currentUser;
 
+  const [page, setPage] = React.useState(0);
+  const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
+  const [orders, setOrders] = useState([]);
 
-    const [page, setPage] = React.useState(0);
-    const [itemsPerPage, setItemsPerPage] = React.useState(optionsPerPage[0]);
-    const [orders, setOrders] = useState([])
+  useEffect(() => {
+    fetch(`https://native-admin.onrender.com/myBooking/?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+      });
+  }, [user.email]);
+  console.log(orders);
 
-    useEffect(() => {
-        fetch(`https://guarded-garden-69209.herokuapp.com/myBooking/?email=${user.email}`)
-            .then(res => res.json())
-            .then(data => {
-                setOrders(data)
-            })
-    }, [user.email])
-    console.log(orders)
+  React.useEffect(() => {
+    setPage(0);
+  }, [itemsPerPage]);
 
-    React.useEffect(() => {
-        setPage(0);
-    }, [itemsPerPage]);
+  return (
+    <DataTable style={{ marginTop: 100 }}>
+      <DataTable.Header>
+        <DataTable.Title>So No</DataTable.Title>
+        <DataTable.Title numeric>Name</DataTable.Title>
+        <DataTable.Title numeric>Email</DataTable.Title>
+        <DataTable.Title numeric>CardNumber</DataTable.Title>
+        <DataTable.Title numeric>Address</DataTable.Title>
+      </DataTable.Header>
 
-    return (
-        <DataTable style={{ marginTop: 100 }}>
+      <FlatList
+        data={orders}
+        keyExtractor={(key) => key._id}
+        renderItem={({ item }) => {
+          return (
+            <>
+              <DataTable.Row>
+                <DataTable.Cell>0</DataTable.Cell>
+                <DataTable.Cell>{item.name}</DataTable.Cell>
+                <DataTable.Cell numeric>{item.email}</DataTable.Cell>
+                <DataTable.Cell numeric>{item.cardNumber}</DataTable.Cell>
+                <DataTable.Cell numeric>{item.address}</DataTable.Cell>
+              </DataTable.Row>
+            </>
+          );
+        }}
+      />
 
-            <DataTable.Header>
-                <DataTable.Title>So No</DataTable.Title>
-                <DataTable.Title numeric>Name</DataTable.Title>
-                <DataTable.Title numeric>Email</DataTable.Title>
-                <DataTable.Title numeric>CardNumber</DataTable.Title>
-                <DataTable.Title numeric>Address</DataTable.Title>
-            </DataTable.Header>
+      <DataTable.Pagination
+        page={page}
+        numberOfPages={3}
+        onPageChange={(page) => setPage(page)}
+        label="1-2 of 6"
+        optionsPerPage={optionsPerPage}
+        itemsPerPage={itemsPerPage}
+        setItemsPerPage={setItemsPerPage}
+        showFastPagination
+        optionsLabel={"Rows per page"}
+      />
+    </DataTable>
+    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //     <Text>Orders</Text>
+    //     <FlatList
+    //         data={orders}
+    //         keyExtractor={(key) => key._id}
+    //         renderItem={({ item }) => {
+    //             return (
+    //                 <View>
+    //                     <Text>{item.name}</Text>
+    //                     <Text>{item.email}</Text>
+    //                     <Text>{item.cardNumber}</Text>
+    //                 </View>
+    //             )
+    //         }}
 
-            <FlatList
-                data={orders}
-                keyExtractor={(key) => key._id}
-                renderItem={({ item }) => {
-                    return (
-                        <>
+    //     />
 
-                            <DataTable.Row>
-                                <DataTable.Cell>0</DataTable.Cell>
-                                <DataTable.Cell>{item.name}</DataTable.Cell>
-                                <DataTable.Cell numeric>{item.email}</DataTable.Cell>
-                                <DataTable.Cell numeric>{item.cardNumber}</DataTable.Cell>
-                                <DataTable.Cell numeric>{item.address}</DataTable.Cell>
-                            </DataTable.Row>
-                        </>
-                    )
-                }}
-
-            />
-
-
-
-
-
-            <DataTable.Pagination
-                page={page}
-                numberOfPages={3}
-                onPageChange={(page) => setPage(page)}
-                label="1-2 of 6"
-                optionsPerPage={optionsPerPage}
-                itemsPerPage={itemsPerPage}
-                setItemsPerPage={setItemsPerPage}
-                showFastPagination
-                optionsLabel={'Rows per page'}
-            />
-        </DataTable>
-        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        //     <Text>Orders</Text>
-        //     <FlatList
-        //         data={orders}
-        //         keyExtractor={(key) => key._id}
-        //         renderItem={({ item }) => {
-        //             return (
-        //                 <View>
-        //                     <Text>{item.name}</Text>
-        //                     <Text>{item.email}</Text>
-        //                     <Text>{item.cardNumber}</Text>
-        //                 </View>
-        //             )
-        //         }}
-
-        //     />
-
-        // </View>
-    )
+    // </View>
+  );
 }
 
-const styles = StyleSheet.create({})
-
+const styles = StyleSheet.create({});
